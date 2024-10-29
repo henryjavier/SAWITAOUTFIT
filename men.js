6a655bdc1a4cec229c59334ea88403c4fe5380e7
HENRY JAVIER HUILLCA AYALA <90071143+henryjavier@users.noreply.github.com>
2024-10-29 17:15:20 -0500

    Update men.js

--- men.js
+++ men.js
@@ -1,39 +1,3 @@
-let slideMen = 0;
-const slides = document.querySelector(".carousel-slider");
-const dots = document.querySelectorAll(".dot");
-
-function showSlide(men) {
-  const offset = -300 * men; // 300px es el ancho de cada imagen
-  slides.style.transform = `translateX(${offset}px)`;
-  dots.forEach((dot, i) => {
-    dot.classList.toggle("active", i === men);
-  });
-}
-
-// Configura la primera imagen activa
-showSlide(slideMen);
-
-// Función para mostrar una imagen específica
-function currentSlide(n) {
-  slideIndex = n - 1;
-  showSlide(slideMen);
-}
-
-// Cambia automáticamente cada 3 segundos
-setInterval(() => {
-  slideMen = (slideMen + 1) % dots.length;
-  showSlide(slideMen);
-}, 3000);
-
-
-
-
-
-
-
-
-
-
 
 let list = document.querySelectorAll(".navigation li");
 
