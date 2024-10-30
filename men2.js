6102f0f55931f5af9e8733548f2114e845dc0fc0
HENRY JAVIER HUILLCA AYALA <90071143+henryjavier@users.noreply.github.com>
2024-10-30 09:51:15 -0500

    Update men2.js

--- men2.js
+++ men2.js
@@ -1,7 +1,6 @@
 let slideIndexes = []; // Arreglo para almacenar el índice de cada carrusel
 const carousels = document.querySelectorAll(".carousel");
 
-// Inicializa cada carrusel
 carousels.forEach((carousel, index) => {
     slideIndexes[index] = 0; // Inicializar el índice de cada carrusel
     showSlide(index, slideIndexes[index]); // Muestra la primera imagen
@@ -12,18 +11,18 @@ function showSlide(carouselIndex, slideIndex) {
     const slides = carousel.querySelectorAll(".carousel-slider img");
     const dots = carousel.querySelectorAll(".dot");
 
-    // Ocultar todas las imágenes y eliminar la clase 'active' de los puntos
+    // Ocultar todas las imágenes y desactivar puntos
     slides.forEach((slide) => (slide.style.display = "none"));
     dots.forEach((dot) => dot.classList.remove("active"));
 
-    // Mostrar la imagen correspondiente y marcar el punto activo
+    // Mostrar la imagen y activar el punto correspondientes
     slides[slideIndex].style.display = "block";
     dots[slideIndex].classList.add("active");
 }
 
-// Función para cambiar a una imagen específica cuando se hace clic en un punto
+// Función para cambiar a una imagen específica al hacer clic en un punto
 function currentSlide(n, carouselIndex) {
-    slideIndexes[carouselIndex] = n - 1; // Ajustar el índice al seleccionar el punto específico
+    slideIndexes[carouselIndex] = n - 1; // Ajustar el índice de la imagen
     showSlide(carouselIndex, slideIndexes[carouselIndex]);
 }
 
@@ -34,3 +33,4 @@ carousels.forEach((carousel, index) => {
         showSlide(index, slideIndexes[index]);
     }, 3000);
 });
+
